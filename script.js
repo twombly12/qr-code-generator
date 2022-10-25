5c7f1f5212a9ef1f9e46493314fa4c95d0598948
Ay Dee <amian.dimichele@gmail.com>
2022-10-25 17:15:35 -0400

    add ability to upload image and render it on screen, still need to overlap with qr code and save as 1 image

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /--- Get Form Values ---/
-const form = document.getElementById('generate-form');
-const qr = document.getElementById('qrcode');
+const form = document.querySelector('#generate-form');
+const qrContent = document.querySelector("#qr-content")
+const qrGenerated = document.querySelector('#qrcode');
 
 /--- Show Spinner ---/
 const showSpinner = () => {
@@ -21,10 +22,28 @@ const generateQRCode = (url, size) => {
     });
 };
 
+/--- Image Input ---/
+
+const imageInput = document.querySelector("#image-input");
+let imageUrl = ''
+imageInput.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        const uploaded_image = reader.result;
+        imageUrl = uploaded_image
+    });
+    reader.readAsDataURL(this.files[0]);
+});
+
 /--- Form Submission ---/
 const onGenerateSubmit = (e) => {
-    const url = document.getElementById('url').value
-    const size = document.getElementById('size').value
+
+    e.preventDefault();
+
+    clearUI()
+
+    const url = document.querySelector('#url-input').value
+    const size = document.querySelector('#size-input').value
 
     console.log(url, size)
 
@@ -35,10 +54,41 @@ const onGenerateSubmit = (e) => {
         setTimeout(() => {
             hideSpinner();
             generateQRCode(url, size)
+            setTimeout(() => {
+                document.querySelector("#uploaded-img").style.backgroundImage = `url(${imageUrl})`;
+                // /--- Add Logo to Canvas ---/
+                // let canvas = document.querySelector("#qrcode canvas");
+                // let ctx = canvas.getContext('2d');
+                // ctx.drawImage(
+                //     imageUrl, 50, 50
+                // );
+
+                /--- Save Button ---/
+                const saveUrl = qrGenerated.querySelector('img').src
+                createSaveBtn(saveUrl)
+            }, 50)
+
         }, 1000);
     }
 }
 
+/--- Clear QR Code --- /
+const clearUI = () => {
+    qrGenerated.innerHTML = "";
+}
+
+/--- Save QR Image ---/
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'save-btn';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Image';
+    qrGenerated.appendChild(link);
+}
+
 hideSpinner()
+
 
 document.querySelector("#qrForm button").addEventListener('click', onGenerateSubmit)
